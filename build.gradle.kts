93b48e599349162866e064128c89c03370e1685b
dhsavell <dhsavell05@gmail.com>
2018-09-26 21:45:18 -0400

    Add test coverage, improve embed DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     base
     kotlin("jvm") version "1.2.41"
+
+    id("jacoco")
+    id("com.github.kt3k.coveralls") version "2.6.3"
     id("org.jmailen.kotlinter") version "1.17.0"
 }
 
@@ -12,6 +15,13 @@ allprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    tasks.withType<JacocoReport> {
+        reports {
+            html.isEnabled = true
+            xml.isEnabled = true
+        }
     }
 
     repositories {
--- wave-core/src/main/kotlin/com/github/dhsavell/wave/core/util/EmbedBuilder.kt
+++ wave-core/src/main/kotlin/com/github/dhsavell/wave/core/util/EmbedBuilder.kt
@@ -18,28 +18,55 @@ annotation class EmbedDsl
 class DslEmbedBuilder {
     val builder = EmbedBuilder()
 
-    fun title(init: DslEmbedBuilder.() -> String) {
+    fun title(init: () -> String) {
         builder.withTitle(init())
     }
 
-    fun description(init: DslEmbedBuilder.() -> String) {
+    fun description(init: () -> String) {
         builder.withDescription(init())
     }
 
-    fun color(init: DslEmbedBuilder.() -> Color) {
+    fun color(init: () -> Color) {
         builder.withColor(init())
     }
 
-    fun section(title: String, description: String, inline: Boolean = false) {
-        builder.appendField(title, description, inline)
+    fun section(init: EmbedSectionBuilder.() -> Unit) {
+        val section = EmbedSectionBuilder().apply(init)
+        builder.appendField(section.title, section.description, section.inline)
     }
 
-    fun footer(init: DslEmbedBuilder.() -> String) {
+    fun footer(init: () -> String) {
         builder.withFooterText(init())
     }
 
     fun build(): EmbedObject {
         return builder.build()
+    }
+
+    @EmbedDsl
+    class EmbedSectionBuilder {
+        var title = ""
+            private set
+        var description = ""
+            private set
+        var inline = false
+            private set
+
+        fun title(init: () -> String) {
+            title = init()
+        }
+
+        fun description(init: () -> String) {
+            description = init()
+        }
+
+        fun notInline() {
+            inline = false
+        }
+
+        fun inline() {
+            inline = true
+        }
     }
 }
 
